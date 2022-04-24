4d18f3c8065d85e1d8d735bb9b9cdade7d62dbdc
Anthony <SkyDaButterRS@gmail.com>
2022-04-24 01:29:22 -0400

    Update karma.js
    more forgotten test changes

--- modules/events/messageCreate/karma.js
+++ modules/events/messageCreate/karma.js
@@ -54,7 +54,7 @@ module.exports = {
             
             if (lastMsg.messages.filter(f => f.channelId == msg.channel.id)[0]) {
                 if (lastMsg.messages.filter(f => f.channelId == msg.channel.id)[0].msg == msg.author.id)
-                    if (msg.content.length < 150) console.log('bypassed lastMsg') //return;
+                    if (msg.content.length < 150) return;
                     else {
                         let msgInd = lastMsg.messages.findIndex(f => f.channelId == msg.channel.id)
                         lastMsg.messages[msgInd].msg = msg.author.id
